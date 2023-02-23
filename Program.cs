3e994b4f6f0f78ea18bd6e1ccf355a08f58005c4
dherrera <dherrera@DIEGOHERRERA>
2023-02-23 17:21:59 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class program
+
+{
+    static void Main(string[] args)
+    {
+        var hora = DateTime.Now;
+        Console.WriteLine(hora.ToString("t"));
+
+        Console.ReadKey();  
+    }
+}
